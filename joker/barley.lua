1b697f2e0133e4606bbf5d09ffeed69740bc3325
threeofaband <sycisytation@gmail.com>
2025-04-01 18:51:43 +0800

    Update barley.lua

--- joker/barley.lua
+++ joker/barley.lua
@@ -3,9 +3,8 @@ SMODS.Joker{
     loc_txt = {
         name = 'Barley Fields',
         text = {
-            '{C:money}+$3{} at end of round',
-            '{C:money}+$1{} interest cap if a',
-            '{C:attention}3{} is scored in hand',
+            '{C:money}+$1{} interest cap when',
+            'a {C:attention}3{} is scored',
             '{C:inactive}Currently{} {C:attention}+$#1#{} {C:inactive}cap{}',
             '{s:0.6}cash crop for a reason.{}'
         },
@@ -26,13 +25,11 @@ SMODS.Joker{
     config = { extra = {interest = 0}},
     pos = {x = 9, y = 1},
 	calculate = function(self, card, context)
-        if context.individual and context.cardarea == G.play  and not context.blueprint then
+        if context.individual and context.cardarea == G.play and not context.blueprint then
             if context.other_card:get_id() == 3 then
                 local cap = G.GAME.interest_cap
-                G.E_MANAGER:add_event(Event({func = function()
-                    G.GAME.interest_cap = cap + 5
-                    return true end }))
-                card.ability.extra.interest = (G.GAME.interest_cap/5)-4
+                G.GAME.interest_cap = cap + 5
+                card.ability.extra.interest = (G.GAME.interest_cap/5)-5
                 return {
                     message = "+!",
                     card = card
@@ -42,8 +39,5 @@ SMODS.Joker{
         if (context.selling_card or context.remove_playing_cards) and context.card == card and not context.blueprint then
             G.GAME.interest_cap = G.GAME.interest_cap - (card.ability.extra.interest*5)
         end
-    end,
-    calc_dollar_bonus = function(self, card)
-        return 3
     end
 }
