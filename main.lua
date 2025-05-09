89012d00275603c9d8bd410875225d727264f30a
threeofaband <sycisytation@gmail.com>
2025-05-10 00:03:07 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,10 +7,6 @@ SMODS.Atlas{
     py = 95
 }
 
-SMODS.current_mod.optional_features = {
-  cardareas = {unscored = true}
-}
-
 B.enabledJokers = {
 'babel',
 'baitandswitch',
@@ -40,15 +36,10 @@ B.enabledJokers = {
 'brokerdealer',
 }
 
--- Register the jokers in custom order
 for i = 1, #B.enabledJokers do
-  local status, err = pcall(function()
+  pcall(function()
     return NFS.load(SMODS.current_mod.path .. '/joker/' .. B.enabledJokers[i] .. '.lua')()
   end)
-  sendDebugMessage('Loaded joker: ' .. B.enabledJokers[i], 'JokerHub')
-  if not status then
-    error(B.enabledJokers[i] .. ': ' .. err)
-  end
 end
 
 assert(SMODS.load_file('challenges.lua'))()
