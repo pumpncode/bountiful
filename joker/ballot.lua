c8b28a1b73bddac14bb98dd154e65592ea200874
threeofaband <sycisytation@gmail.com>
2025-05-28 10:05:09 +0800

    Update ballot.lua

--- joker/ballot.lua
+++ joker/ballot.lua
@@ -12,43 +12,27 @@ SMODS.Joker {
     atlas = 'jokers',
     pos = { x = 0, y = 1 },
     rarity = 1,
-    config = {
-        extra = {
-            chipgain = 0,
-            repetitions = 0
-        }
-    },
+    config = { extra = { chips = 0, chip_mod = 0 } },
     blueprint_compat = true,
     cost = 6,
     loc_vars = function(self, info_queue, card)
-        return {
-            vars = { card.ability.extra.chipgain }
-        }
+        return { vars = { card.ability.extra.chipgain } }
     end,
     set_badges = function(self, card, badges)
         badges[#badges+1] = create_badge('suggested by @intersectingplanes', G.C.BLACK, G.C.WHITE, 0.8 )
     end,
     calculate = function(self, card, context)
-        local bal = card.ability.extra
         if context.individual and context.cardarea == G.play and not context.blueprint then
-            for i=1, #G.play.cards do
-                bal.chipgain = bal.chipgain + 1
-                bal.repetitions = bal.repetitions + 1
-                return {
-                    G.play.cards[i]:juice_up(0.5, 0.5),
-                    card = card,
-                    message = '+1',
-                    colour = G.C.CHIPS,
-                }
-            end
-        end
-        if context.joker_main and bal.repetitions > 0 then
+            card.ability.extra.chips = card.ability.extra.chips + card.ability.extra.chip_mod
+            card:juice_up(0.5, 0.5),
             return {
-                chips = bal.chipgain,
+                message = '+'..card.ability.extra.chip_mod,
+                colour = G.C.CHIPS,
             }
-        end
-        if context.before or context.after then
-            bal.repetitions = 0
+        elseif context.joker_main then
+            return {
+                chips = card.ability.extra.chips
+            }
         end
     end
 }
