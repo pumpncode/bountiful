9e7b28807a213610378880337563c23ae702eb9a
threeofaband <sycisytation@gmail.com>
2025-05-28 09:56:47 +0800

    Update basicity.lua

--- joker/basicity.lua
+++ joker/basicity.lua
@@ -5,8 +5,7 @@ SMODS.Joker{
         text = {
             '{C:blue}Common{} Jokers give',
             '{C:chips}+14{} Chips and {C:mult}+3{} Mult',
-            '{s:0.6}7 in 15 people are basic{}',
-            '{s:0.6}according to litmus tests.{}'
+            '{s:0.6}7 in 15 people are basic{}
         },
     },
     atlas = 'jokers',
@@ -17,21 +16,21 @@ SMODS.Joker{
     blueprint_compat = true,
     eternal_compat = true,
     perishable_compat = true,
-    config = { extra = { mult = 0, chips = 0 }},
+    config = { extra = { mult_mod = 3, chip_mod = 14 }},
     pos = {x = 9, y = 0},
     calculate = function(self, card, context)
         if context.joker_main then
-            card.ability.extra.mult = 0
-            card.ability.extra.chips = 0
+            local mult = 0
+            local chips = 0
             for i = 1, #G.jokers.cards do
                 if G.jokers.cards[i].config.center.rarity == 1 then
-                    card.ability.extra.mult = card.ability.extra.mult + 3
-                    card.ability.extra.chips = card.ability.extra.chips + 14
+                    mult = mult + card.ability.extra.mult_mod
+                    chips = chips + card.ability.extra.chip_mod
                 end
             end
             return {
-                mult = card.ability.extra.mult,
-                chips = card.ability.extra.chips
+                mult = mult,
+                chips = chips
             }
         end
     end
