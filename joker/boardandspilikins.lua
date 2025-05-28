8f77f523ddfff541e93680434ebc5f45af3b5b76
threeofaband <sycisytation@gmail.com>
2025-05-28 10:13:41 +0800

    Update boardandspilikins.lua

--- joker/boardandspilikins.lua
+++ joker/boardandspilikins.lua
@@ -15,25 +15,19 @@ SMODS.Joker{
     unlocked = true,
     discovered = false,
     blueprint_compat = true,
-    eternal_compat = true,
-    perishable_compat = true,
-    config = { extra = { mult = 0 }},
+    config = { extra = { mult = 15 }},
     pos = {x = 1, y = 0},
     set_badges = function(self, card, badges)
         badges[#badges+1] = create_badge('suggested by @ironic_', G.C.BLACK, G.C.WHITE, 0.8 )
     end,
-	calculate = function(self, card, context)
+    calculate = function(self, card, context)
         if context.individual and context.cardarea == G.play then
             local rank = context.other_card:get_id()
-            card.ability.extra.mult = 0
             for i=1, #context.full_hand do
-                if context.full_hand [i]:get_id() == 11 then
-                    card.ability.extra.mult = card.ability.extra.mult + 15
-                    if rank == 5 then
-                        return {
-                            mult = card.ability.extra.mult
-                        }
-                    end
+                if context.full_hand[i]:get_id() == 11 and rank == 5 then
+                   return {
+                      mult = card.ability.extra.mult
+                   }
                 end
             end
         end
