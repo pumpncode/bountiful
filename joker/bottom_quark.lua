39fd8aa9217646313895cf06bd192ba8434df071
threeofaband <sycisytation@gmail.com>
2025-05-30 17:11:31 +0800

    Update bottom_quark.lua

--- joker/bottom_quark.lua
+++ joker/bottom_quark.lua
@@ -19,17 +19,17 @@ SMODS.Joker{
    blueprint_compat = false,
    eternal_compat = true,
    perishable_compat = true,
-   config = { extra = { chance = 4, first = 0 }},
+   config = { extra = { set = false }},
    pos = {x = 2, y = 2},
 	calculate = function(self, card, context)
-      if card.ability.extra.first == 0 and G.GAME.spectral_rate < 2 and not context.blueprint then
+      if not card.ability.extra.set and G.GAME.spectral_rate < 2 and not context.blueprint then
          G.GAME.spectral_rate = G.GAME.spectral_rate + 2
-         card.ability.extra.first = 1
+         card.ability.extra.set = true
       end
       if (context.selling_card or context.remove_playing_cards) and context.card == card and card.ability.extra.first ~= 0 and not context.blueprint then
          G.GAME.spectral_rate = G.GAME.spectral_rate - 2
       end
-      if context.setting_blind then
+      if context.setting_blind and not context.blueprint then
          for k, v in ipairs(G.consumeables.cards) do
             if v.config.center_key and v.ability.consumeable then
                if v.config.center.set == 'Spectral' then
