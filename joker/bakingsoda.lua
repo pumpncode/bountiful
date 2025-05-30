5821c422b383c71570e252023ecbb9fec38f2240
threeofaband <sycisytation@gmail.com>
2025-05-30 16:42:13 +0800

    Update bakingsoda.lua

--- joker/bakingsoda.lua
+++ joker/bakingsoda.lua
@@ -21,7 +21,7 @@ SMODS.Joker{
         }
     },
     blueprint_compat = true,
-    eternal_compat = true,
+    eternal_compat = false,
     cost = 4,
     loc_vars = function(self, info_queue, card)
         return {
