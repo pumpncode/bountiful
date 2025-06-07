ce105de5ca82f9f98cd80c30dc7c10d87e35c997
threeofaband <sycisytation@gmail.com>
2025-06-07 22:32:04 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,26 +1,33 @@
 B = {}
 
 SMODS.Atlas{
-    key = 'jokers',
-    path = 'atlas.png',
-    px = 71,
-    py = 95
+   key = 'jokers',
+   path = 'atlas.png',
+   px = 71,
+   py = 95
 }
 
 SMODS.Atlas{
-    key = 'blind',
-    path = 'blind.png',
-    px = 34,
-    py = 34,
-    frames = 21,
-    atlas_table = 'ANIMATION_ATLAS'
+   key = 'crossmods',
+   path = 'crossmodatlas.png',
+   px = 71,
+   py = 95
+}
+
+SMODS.Atlas{
+   key = 'blind',
+   path = 'blind.png',
+   px = 34,
+   py = 34,
+   frames = 21,
+   atlas_table = 'ANIMATION_ATLAS'
 }
 
 SMODS.Atlas {
-    key = "modicon",
-    path = "icon.png",
-    px = 34,
-    py = 34
+   key = "modicon",
+   path = "icon.png",
+   px = 34,
+   py = 34
 }
 
 B.enabledJokers = {
@@ -45,6 +52,7 @@ B.enabledJokers = {
 'bitshift',
 'blackmail',
 'blahaj',
+'blatant',
 'boardandspilikins',
 'boarding_ticket',
 'bobber',
@@ -53,15 +61,41 @@ B.enabledJokers = {
 'bourgeoisie',
 'brake',
 'brokerdealer',
-'bulletin',
+'bulletin'
 }
 
+if not G.P_CENTER_POOLS["Food"] then
+   SMODS.ObjectType {
+      key = "Food",
+      default = "j_gros_michel",
+      cards = {},
+      inject = function(self)
+         SMODS.ObjectType.inject(self)
+         self:inject_card(G.P_CENTERS.j_cavendish)
+         self:inject_card(G.P_CENTERS.j_diet_cola)
+         self:inject_card(G.P_CENTERS.j_egg)
+         self:inject_card(G.P_CENTERS.j_gros_michel)
+         self:inject_card(G.P_CENTERS.j_ice_cream)
+         self:inject_card(G.P_CENTERS.j_popcorn)
+         self:inject_card(G.P_CENTERS.j_ramen)
+         self:inject_card(G.P_CENTERS.j_selzer)
+         self:inject_card(G.P_CENTERS.j_turtle_bean)
+      end,
+   }
+end
+
 for i = 1, #B.enabledJokers do
-  pcall(function()
-    return NFS.load(SMODS.current_mod.path .. '/joker/' .. B.enabledJokers[i] .. '.lua')()
-  end)
+   pcall(function()
+      return NFS.load(SMODS.current_mod.path .. '/joker/' .. B.enabledJokers[i] .. '.lua')()
+   end)
 end
 
 assert(SMODS.load_file('challenges.lua'))()
 assert(SMODS.load_file('decks.lua'))()
 assert(SMODS.load_file('blinds.lua'))()
+
+if next(SMODS.find_mod("pta_saka")) then
+   pcall(function()
+      return NFS.load(SMODS.current_mod.path .. '/crossmods/haya.lua')()
+   end)
+end
